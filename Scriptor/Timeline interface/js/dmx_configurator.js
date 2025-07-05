487feca00b2f9b599c531e3684d2a47ead42a4d0
Atom Energy <justmerrivera@gmail.com>
2025-07-05 09:25:57 +0800

    Implemeneted Full Config editor, some prep for storage saving

--- Scriptor/Timeline interface/js/dmx_configurator.js
+++ Scriptor/Timeline interface/js/dmx_configurator.js
@@ -12,6 +12,12 @@ if(typeof(_) == 'undefined'){
 	}
 }
 
+if(typeof(make) == 'undefined'){
+   function make(elm){
+		return document.createElement(elm);
+	}
+}
+
 
 
 function clone(obj) {
@@ -34,6 +40,7 @@ let options = {};
 
 let SAVES = []; //stores the saves data
 let selectedIndex = 0;
+let selectedSaveIndex = 0;
 
 // ========================
 //Constant data structure
@@ -59,7 +66,7 @@ const OPTIONS_DATA = {
 
 
 const DMX_CONF = {
-	
+	mode: "add",
 	loadAllSaved: function(){
 		let saves = localStorage.getItem("DMX_SAVES");
 		if(saves == null || saves.length <= 0){
@@ -80,9 +87,21 @@ const DMX_CONF = {
 	},
 	
 	//To-Do: Add data to save items
-	addToSaves: function(data){
+	addToSaves: function(){
+		let DataOptions = clone(OPTIONS_DATA);
+		let configsData = clone(configs);
 		
+		let data = {
+			options: DataOptions,
+			configs: configsData,
+		}
 		
+		if(selectedSaveIndex == undefined){
+			SAVES.push(data);
+		}else{
+			SAVES[selectedSaveIndex] = data;
+		}
+		let saves = localStorage.setItem("DMX_SAVES", JSON.stringify(SAVES));
 		
 	},
 	
@@ -92,12 +111,106 @@ const DMX_CONF = {
 		
 		
 	},
-	//Configurator Logic Goes Down here
 	
-	openConfigWindow: function(data){
-		_("configModal").showModal();
+	renderConfigs: function(){
 		
-		_("configModal").addEventListener("cancel", this.closeDefaultModal)
+		_("configs_tab_section").innerHTML = "";
+		
+		for(let z = 0; z < configs.length;z++){
+			let confData = configs[z];
+			let config_tab_template = _("config_tab_template").cloneNode(true).content;
+			
+			if(!confData){
+				return false;
+			}
+			
+			let channel_config = (config_tab_template.querySelector('[tag="channel_config"]'));
+			
+			let dmx_config = (config_tab_template.querySelector('[tag="dmx_config"]'));
+			
+			let remove_thistab = (config_tab_template.querySelector('[tag="remove_thistab"]'));
+			
+			
+			channel_config.innerText = confData.channel;
+			
+			for(each of confData.dmx_config){
+				let pods = make('span');
+					pods.innerText = each.type + each.target;
+					pods.title = this.getFeatureNameByChar(each.type);
+					dmx_config.appendChild(pods);
+			}
+			
+			remove_thistab.setAttribute("index", z);
+			dmx_config.setAttribute("index", z);
+			
+			_("configs_tab_section").appendChild(config_tab_template);
+			
+			
+		}
+		
+		
+	},
+	
+	
+	removeThistab: function(elm){
+		let ownindex = elm.getAttribute("index");
+		
+		let confirmDelete = confirm("Are you sure to remove this item?");
+		if(!confirmDelete){
+			return;
+		}
+		configs.splice(ownindex,1);
+		this.renderConfigs();
+	},
+	
+	//helper function for position
+	getFeatureNameByChar: function (char) {
+		const featureMap = {
+			c: "Color",
+			p: "Position",
+			f: "Fader",
+			a: "Feature A",
+			b: "Feature B",
+			d: "Feature D",
+			e: "Feature E",
+			g: "Feature G",
+			h: "Feature H",
+			i: "Feature I",
+			j: "Feature J",
+			k: "Feature K"
+		};
+
+		return featureMap[char] || "Unknown Feature";
+	},
+		
+		
+	// ===============================	
+	//Configurator Logic Goes Down here
+	// ===============================	
+	
+	openConfigWindow: function(edit=false){
+		_("configModal").showModal();
+		_("configModal").addEventListener("cancel", this.closeDefaultModal);
+		if(edit){
+			this.mode = "edit";
+		}else{
+			this.mode = "add";
+		}
+		
+	},
+	
+	openForEdit: function(elm){
+		let index = elm.getAttribute("index");
+		selectedIndex = index;
+		let confData = configs[selectedIndex];
+		
+		this.clearAllEditTabs();
+		for(each of confData.dmx_config){
+			this.addConfigEdit(each);
+		}
+		
+		this.openConfigWindow(true);
+		_("conf_channel").value = confData.channel;
 		
 	},
 	
@@ -134,17 +247,33 @@ const DMX_CONF = {
 			}
 			
 			configData.dmx_config.push(objectConf);
-			configs[selectedIndex] = configData;
+	
 			
-			// To-Do: Close Modal and Reset it
+			
 			
 		}
 		
+				
+		if(this.mode == "add"){
+			configs.push(configData);
+		}else{
+			configs[selectedIndex] = configData;
+		}
+		
+		_("conf_channel").value = 0;
+		this.renderConfigs();
+		this.clearAllEditTabs();
+		this.closeModal();
 
 		
 	},
 	
+	
+	
+	// ===========================
 	//Config Editor Section
+	// ===========================
+	
 	
 	//Adds the Config tab into the editor, if data is passed, it puts the value as well
 	addConfigEdit: function(data){
@@ -183,12 +312,10 @@ const DMX_CONF = {
 DMX_CONF.loadAllSaved();
 
 
-DMX_CONF.openConfigWindow();
+// DMX_CONF.openConfigWindow();
+
 DMX_CONF.addConfigEdit();
 
 
-
-
-
-
-
+// dummy 
+DMX_CONF.renderConfigs();
