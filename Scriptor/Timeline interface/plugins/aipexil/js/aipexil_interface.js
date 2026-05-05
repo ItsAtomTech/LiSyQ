31f53edeb95148392b7b7208ba07bec33b0f9a36
Atom Energy <justmerrivera@gmail.com>
2026-05-06 06:55:42 +0800

    Updated aipexil Plugin with the New Strobe and more effects for the solid color FX selection

--- Scriptor/Timeline interface/plugins/aipexil/js/aipexil_interface.js
+++ Scriptor/Timeline interface/plugins/aipexil/js/aipexil_interface.js
@@ -750,23 +750,29 @@ function gen_end_val(rd){
 
 
 
-
 function playon(){
 	
 	if(preview_play){
 		
-		
-		
 		// _("preview").style.backgroundColor = "#"+colors_array[at_point];
-		
-		//console.log(aipexil.seqData[at_point]);
 		
 		//To-DO: Interactive preview based on type detected
 		
-		_("preview_container").innerText = "No visual preview for this effect! \n" +aipexil.seqData[at_point];
+		try{
+			if(aipexil.seqData[at_point].startsWith("fx:color")){
+				let color = aipexil.seqData[at_point].split(":")[2];
+				_("preview_container").innerText = aipexil.seqData[at_point];
+				_("preview_container").style.backgroundColor = "#"+color+"ee";
+			}else{
+				_("preview_container").style.backgroundColor = "unset";
+				_("preview_container").innerText = "No visual preview for this effect! \n" +aipexil.seqData[at_point];
+			}
+			
+		}catch(e){
+			//--
+		}
 		
-		
-		
+
 		
 		preview_head(at_point);
 		
@@ -781,8 +787,6 @@ function playon(){
 		}
 	}
 
-	
-	
 	
 }
 
@@ -866,7 +870,9 @@ function dual_value(elm,form_id, index){
 	}
 	
 
-	
+	if(elm.getAttribute("color") == "true"){
+		elm.style.color = _(form_id).value;
+	}
 	
 	
 	elm.innerText = text +  _(form_id).value;
@@ -1035,7 +1041,14 @@ function generate_premade_inputs(type){
 						
 						from_.innerText =  "From: " + selected_stabs.effect_params.values[indexes][0];
 						
+						if(selected_stabs.effect_params.fx_type == "color"){
+							from_.style.color = selected_stabs.effect_params.values[indexes][0];
+							from_.setAttribute("color","true");
+						};
+						
 					}
+				
+				
 				
 					
 				let to_ = document.createElement("button");
@@ -1048,6 +1061,12 @@ function generate_premade_inputs(type){
 					if(mode == "edit" && typeof(selected_stabs.effect_params.values[indexes]) == "object"){
 						
 						to_.innerText = "To: " + selected_stabs.effect_params.values[indexes][1];
+						
+						if(selected_stabs.effect_params.fx_type == "color"){
+							to_.style.color = selected_stabs.effect_params.values[indexes][1];
+							to_.setAttribute("color","true");
+						};
+						
 						
 					}
 					
@@ -1245,7 +1264,7 @@ function generate_template(){
 	sendTo("stop_media_prev");
 	sendTo('close_plugin','');
 	
-	
+	return template_data;
 }
 
 
@@ -1276,7 +1295,3 @@ function load_from_host(df){
 
 	
 }
-
-
-
-
--- Scriptor/Timeline interface/plugins/aipexil/js/generator.js
+++ Scriptor/Timeline interface/plugins/aipexil/js/generator.js
@@ -1,5 +1,5 @@
 // var aipexil_time_line = [];
-
+// aipexil v1.3
 var timing = 33.33; //30 fps
 
 
@@ -11,9 +11,69 @@ const aipexil = {
 	seqData: [],
 	key_frames: [],
 	
+	pulse_effect: false,
+    strobe_effect: false,
+    other_effect: "none",
+
+    fade_counter: 1,
+    fade_amount: 1,
+    effect_len: 1,
+	
+	effect_banks: ["none", "strobe", "pulse", "beating"],
 	
 	aipexil_time_line:[],
 	premade_effects_params:[],
+	
+	
+	
+//effects logics starts =======
+
+  
+    keyframe_fade_counter: 1,
+    keyframe_fade_amount: 1,
+    keyframe_effect_len: 1,
+	keyframe_effect: "",
+
+	beating_effect: {
+	  beat_time: 0,
+	  thresehold: 33,
+	  effect_amount: 35,
+
+	  beat: function (interval) {
+		  this.beat_time += 1 / interval;
+	  },
+
+	  get_beat: function () {
+		  if (this.beat_time > this.thresehold) this.beat_time = 0;
+		  return 1 - ((this.beat_time / this.thresehold) * this.effect_amount * 0.01);
+	  },
+
+	  auto_beat: function (inter) {
+		  this.beat(inter);
+		  return this.get_beat();
+	  },
+
+	  reset_clk: function () {
+		  this.beat_time = 0;
+	  }
+	  
+  },
+
+
+	// strobe effect	
+	  strobed_ambient: function (fade_length, keyframed = false) {
+		  let fade = keyframed ? this.keyframe_fade_amount : this.fade_amount;
+		  return fade < 0 ? 0 : 1;
+	  },	
+
+	// pulse effect  
+	  pulsed_ambient: function (fade_length, keyframed = false) {
+		return keyframed
+			? this.keyframe_fade_counter / fade_length
+			: this.fade_counter / fade_length;
+	  },
+	
+//effects ends here ===============	
 	
 	//here are the premade effects which we loop through to create forms from
 	premade_effects_bin:{
@@ -58,7 +118,7 @@ const aipexil = {
 							{
 								name:"Effect threshold",
 								type: "number",
-								min: 0,
+								min: -1,
 								max: 5,
 								default: 0.5,
 								animatable: false,
@@ -196,13 +256,6 @@ const aipexil = {
 			
 		},
 		
-		
-		
-		
-		
-		
-		
-		
 	
 	},
 
@@ -217,8 +270,16 @@ const aipexil = {
 		
 		let prevVal;
 		
-	for(i=0; i<= limit; i++) {
+		this.fade_amount = 1;
+        this.fade_counter = 1;
+		this.beating_effect.reset_clk();
 
+        this.keyframe_fade_counter = 1;
+        this.keyframe_fade_amount = 1;
+		
+		
+	for(i=0; i<= limit; i++) {
+		
 		this.seqData[i] = " ";
 		for(x=0; x < this.key_frames.length;x++){
 
@@ -254,7 +315,7 @@ const aipexil = {
 														
 								param_val = parseInt(aipexil.linear_flow(start_val,end_val,zlen,time_tick));			
 								
-								
+							//if param animatable amd is color
 							}else if(typeof(param_val) == 'object' && (this.premade_effects_bin[params_.fx_type].params[zg].animatable && this.premade_effects_bin[params_.fx_type].params[zg].type == "color")){
 									
 								let start_val = (params_.values[zg][0]) || "000000";
@@ -262,7 +323,7 @@ const aipexil = {
 								let zlen = parseInt(this.key_frames[x].time_end*this.time_constant) - this.key_frames[x].time_start*this.time_constant;
 								let all_params = this.premade_effects_bin[params_.fx_type].params[zg];
 
-								param_val = aipexil.processColor(start_val,end_val,zlen,time_tick,all_params);
+								param_val = aipexil.processColor(start_val,end_val,zlen,time_tick,this.key_frames[x]);
 
 
 							};
@@ -286,7 +347,6 @@ const aipexil = {
 								stringVal = stringVal+ ":" + param_val;
 							}
 							
-											
 
 							}catch(e){
 								// console.error(e);
@@ -309,6 +369,27 @@ const aipexil = {
 				}
 					
 			}
+			
+			
+			//calculate effect params for e.g colors etc.
+			
+			if (["pulse", "strobe"].includes(this.keyframe_effect)) {
+                if (this.keyframe_fade_counter < 1 || this.keyframe_fade_counter > this.keyframe_effect_len) {
+                    this.keyframe_fade_amount = -this.keyframe_fade_amount;
+                }
+                this.keyframe_fade_counter += this.keyframe_fade_amount;
+            }
+
+
+            if (this.keyframe_effect == "beating") {
+                this.beating_effect.beat(this.keyframe_effect_len / 33);
+            }
+			
+			
+			
+            this.fade_counter += this.fade_amount;  
+		  
+			
 			
 		}
 	},
@@ -371,10 +452,17 @@ const aipexil = {
 
 
 	//Process the color hex into its split and calculate value
-	processColor: function(start_val,end_val,zlen,time_tick){
+	processColor: function(start_val,end_val,zlen,time_tick,keyframe_params){
 		let color_hex = "000000";
 		let start_color_array = this.split_solor(this.removeSpecials(start_val));
 		let end_color_array = this.split_solor(this.removeSpecials(end_val));
+	
+		let key_effect = aipexil.effect_banks[keyframe_params.effect_params.values[2]] || "none";
+		let effect_thres = keyframe_params.effect_params.values[3] || 0;
+	
+		this.keyframe_effect_len = parseFloat(effect_thres) * 33;
+		this.keyframe_effect = key_effect;
+		
 		
 		let calculated_color = 	[];
 			let cind = 0;
@@ -387,11 +475,37 @@ const aipexil = {
 					
 					calculated_color[cind] = this.normalize_ambient(this.linear_flow(x,y,z,w));
 
-
-
+					//Apply Effect to calculated value.
+					try {
+                            if (key_effect == "pulse") {
+                                calculated_color[cind] = this.normalize_ambient(
+                                    parseInt(calculated_color[cind] * this.pulsed_ambient(this.keyframe_effect_len, true))
+                                );
+                            } else if (key_effect === "strobe") {
+                                calculated_color[cind] = parseInt(
+                                    calculated_color[cind] * this.strobed_ambient(this.keyframe_effect_len, true)
+                                );
+                            } else if (key_effect === "beating") {
+                                calculated_color[cind] = parseInt(
+                                    calculated_color[cind] * this.beating_effect.get_beat(this.keyframe_effect_len / 33)
+                                );
+                            }
+							
+						
+							
+					  } catch (e) {
+						  // console.log(e);
+					  }
+					
 					cind++;
 			}
 
+			
+			
+			// console.log(keyframe_params.effect_params.values, key_effect, effect_thres);
+
+			
+			// console.log(this.keyframe_fade_counter);
 
 			color_hex = this.ambient_combined_hex(calculated_color);
 
@@ -412,6 +526,7 @@ const aipexil = {
 		this.timelen = data_delinked.seconds_length;
 		this.seqData.length = 0;	
 		
+
 		aipexil.generateData();
 		
 		return this.seqData;
@@ -430,3 +545,10 @@ function regen_f3e9cc2243ff902c80b908829745750d(data){
 	return aipexil.regenerate(data);
 	
 }
+
+
+
+
+
+
+
